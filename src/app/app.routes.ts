6fd7157731c508bd246607fdb3de936d7496c161
Abdallah Elgendy <abdallah.saad.elgendy@gmail.com>
2023-12-08 15:44:24 +0200

    Create login screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MapComponent } from './map/map.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-  { path: "", component: MapComponent },
+  { path: "", component: LoginComponent },
 ];
