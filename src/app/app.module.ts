5adb41c751bf7b3ebe2e5d9f0d2c64a38610a84f
Mostafa Hamdy <73959716+MostafaHamdy3@users.noreply.github.com>
2023-12-08 16:01:21 +0200

    Add intialize dashboard screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    MatIconModule,
     AgmCoreModule.forRoot({
       apiKey: ""
     })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MapComponent } from './map/map.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: "", component: MapComponent },
+  { path: "", component: DashboardComponent },
 ];
