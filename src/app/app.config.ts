241ae04a5afaa440ccabff9e6659b4a133855006
Mostafa Hamdy <73959716+MostafaHamdy3@users.noreply.github.com>
2023-12-17 23:26:13 +0200

    Remove duplicate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
